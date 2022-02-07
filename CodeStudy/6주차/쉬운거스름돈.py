385b2dfba1ef983fd2f391056441401dfd1b279f
joey0807 <joey0807@naver.com>
2022-02-07 20:43:23 +0900

    code study

--- CodeStudy/6주차/쉬운거스름돈.py
+++ CodeStudy/6주차/쉬운거스름돈.py
@@ -38,3 +38,17 @@ for i in range(T):
     
     print('#{}'.format(i+1))
     print('{} {} {} {} {} {} {} {}'.format(a,b,c,d,e,f,g,h))
+
+#다른 사람이 푼 깔끔해보이는 풀이
+# T = int(input())
+
+# for t in range(1, T+1) :
+#     N = int(input())
+#     money = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+#     list = [0] * 8
+#     for i in range(8) :
+#         if N//money[i] != 0 :
+#             list[i] = N//money[i]
+#             N = N%money[i]
+#     print("#{}".format(t))
+#     print(*list)
