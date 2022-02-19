3e51dc189956756e54d1b540b57eee4411a2876e
joey0807 <joey0807@naver.com>
2022-02-19 20:14:17 +0900

    Code Study

--- CodeStudy/8주차/거듭제곱.py
+++ CodeStudy/8주차/거듭제곱.py
@@ -1,12 +1,12 @@
-def involution(a,b):
-       if b == 1:
-           return a
-       return involution(a,b-1)*a
+def involution(a,b): #거듭제곱 재귀함수
+       if b == 1: #두번째로 입력받은 변수가 1이 될 때 까지 반복
+           return a #결과값 리턴
+       return involution(a,b-1)*a #두번째 변수를 -1 해주고 a*a
 
 
-for _ in range(10):
-    N = int(input())
-    x, y = map(int,input().split())
+for _ in range(10): #테스트케이스 10개
+    N = int(input()) 
+    x, y = map(int,input().split()) #x^y 변수 값 입력
 
 
     print('#{} {}'.format(N,involution(x,y)))
