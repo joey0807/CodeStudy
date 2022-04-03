bd12d6b08c9d6bb52a3bf3542536f596790ac779
joey0807 <joey0807@naver.com>
2022-04-04 00:39:46 +0900

    Code Study

--- CodeStudy/14주차/카드카운팅.py
+++ CodeStudy/14주차/카드카운팅.py
@@ -1,48 +1,30 @@
 T = int(input())
 for i in range(T):
-    card = list(input())
+    card = list(input()) #카드 정보
 
-    card_list = []
-    spade = 0
-    dia = 0
-    heart = 0
-    clover = 0
+    card_list = [] #카드 정보를 구분해서 받을 리스트
+    spade = 0 #스페이드 카드 개수
+    dia = 0 #다이아몬드 카드 개수
+    heart = 0 #하트 카드 개수
+    clover = 0  #클로버 하트 개수
 
-    for x in range(0,len(card),3):
-        card_list.append(card[x:x+3])
-        if card_list[-1] == card[x:x+3]:
-            print('#{} ERROR')
-        else:
-            if card[x] == 'S':
-                spade += 1
-            elif card[x] == 'D':
+    for x in range(0,len(card),3): #card 문자열을 3개씩 나눠서 입력받음
+        if card[x:x+3] not in card_list: #입력받는 card 문자열이 card_list에 없다면
+            card_list.append(card[x:x+3]) #card_list에 추가
+        else: #있다면 중복되는 거니까
+            print('#{} ERROR'.format(i+1)) #에러 문구 출력하고
+            break #멈춤
+    else: #for-else문 : for문이 중간에 break로 끊기지 않고 수행되었을 때 수행하는 코드
+        for x in card: #card 문자열을 비교
+            if x == 'S': #S가 포함되어 있으면 스페이드 카드 개수 +1
+                spade += 1 
+            elif x == 'D': #D가 포함되어 있으면 다이아몬드 카드 개수 +1
                 dia += 1
-            elif card[x] == 'H':
+            elif x == 'H': #H가 포함되어 있으면 하트 카드 개수 +1
                 heart += 1
-            elif card[x] == 'C':
+            elif x == 'C': #C가 포함되어 있으면 클로버 카드 개수 +1
                 clover += 1
+            else: #나머지 문자열은 통과
+                continue
 
-            
-    #print('#{} {} {} {} {}'.format(i+1,13-spade,13-dia,13-heart,13-clover))
-
-    # card1 = card[0:3]
-    # card2 = card[3:6]
-    # card3 = card[6:9]
-    # card4 = card[9:]
-
-    # if card1 == card2 or card1 ==card3 or card1 == card4 or card2 ==card3 or card2 == card4 or card3 == card4:
-    #     print('#{} ERROR'.format(i+1))
-    # else:
-    #     for x in card:
-    #         if x == 'S':
-    #             spade += 1
-    #         elif x == 'D':
-    #             dia += 1
-    #         elif x == 'H':
-    #             heart += 1
-    #         elif x == 'C':
-    #             clover += 1
-    #         else:
-    #             continue
-        
-    #     print('#{} {} {} {} {}'.format(i+1,13-spade,13-dia,13-heart,13-clover))
+        print('#{} {} {} {} {}'.format(i+1,13-spade,13-dia,13-heart,13-clover)) #계산된 카드 개수 출력
