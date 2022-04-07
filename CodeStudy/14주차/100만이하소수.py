dca081c758e64b2c4e3f959ba82cb18118c7c8ab
joey0807 <82745321+joey0807@users.noreply.github.com>
2022-04-07 20:10:15 +0900

    code study

--- CodeStudy/14주차/100만이하소수.py
+++ CodeStudy/14주차/100만이하소수.py
@@ -12,4 +12,4 @@ for i in range(2,n+1): #2부터 n까지
     primes.append(i) #소수니까 primes 리스트에 i 추가
     for j in range(2*i, n+1, i): #i를 소수로 채택했으니 i의 배수들을 모두 False로 바꿈
         a[j] = False
-print(primes) #primes 리스트 출력
+print(*primes) #primes 리스트 출력
--- CodeStudy/14주차/신뢰.py
+++
@@ -1,6 +0,0 @@
-T = int(input())
-for i in range(T):
-    N = int(input())
-    button = input().split()
-
-    print(button)
