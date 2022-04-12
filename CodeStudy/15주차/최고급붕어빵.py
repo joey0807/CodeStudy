155b8272c4dce0b50645c093a4faf6f256381138
joey0807 <82745321+joey0807@users.noreply.github.com>
2022-04-12 23:17:41 +0900

    code study

---
+++ CodeStudy/15주차/최고급붕어빵.py
@@ -0,0 +1,9 @@
+T = int(input())
+for i in range(T):
+    n, m, k = map(int, input().split())
+    customer = []
+    for x in range(n):
+        a = int(input())
+        customer.append(a)
+
+    
